725633fafa04c5a8db7ebaf69ca358f16a318175
Jordan Olguin <jdolguin62@gmail.com>
2025-03-29 13:50:12 -0600

    updated styles of StudentCard and StudentList

--- client/src/components/StudentCard/StudentCard.js
+++ client/src/components/StudentCard/StudentCard.js
@@ -1,13 +1,18 @@
 import styles from './StudentCard.module.css';
-import { FaEdit, FaTrash } from 'react-icons/fa';
+import { FaEdit, FaTrash, FaUserGraduate } from 'react-icons/fa';
 
 const StudentCard = ({ student, onEdit, onDelete }) => {
   return (
     <div className={styles.card}>
-      <h3 className={styles.name}>
-        {student.firstName} {student.lastName}
-      </h3>
-      <p className={styles.grade}>Grade: {student.grade}%</p>
+      <div className={styles.iconWrapper}>
+        <FaUserGraduate className={styles.studentIcon} />
+      </div>
+      <div className={styles.details}>
+        <h3 className={styles.name}>
+          {student.firstName} {student.lastName}
+        </h3>
+        <p className={styles.grade}>Grade: {student.grade}%</p>
+      </div>
       <div className={styles.actions}>
         <button className={styles.editBtn} onClick={() => onEdit(student)}>
           <FaEdit className={styles.icon} /> Edit

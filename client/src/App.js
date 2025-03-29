7e0a3f0b80e0f94152a753c5420a0ef075168d3d
Jordan Olguin <jdolguin62@gmail.com>
2025-03-29 12:52:46 -0600

    initial instance of dashboard with dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
+import { useState } from 'react';
+import StudentList from './components/StudentList/StudentList';
 import './App.css';
 
+const dummyStudents = [
+  {
+    _id: '1',
+    firstName: 'Emma',
+    lastName: 'Johnson',
+    grade: 92,
+  },
+  {
+    _id: '2',
+    firstName: 'Liam',
+    lastName: 'Garcia',
+    grade: 85,
+  },
+];
+
 function App() {
+  const [students, setStudents] = useState(dummyStudents);
+
+  const handleDelete = (id) => {
+    const updateStudents = students.filter((student) => student._id !== id);
+    setStudents(updateStudents);
+  };
+
+  const handleEdit = (student) => {
+    console.log('Editing student:', student);
+  };
+
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className='appContainer'>
+      <h1 className='appTitle'>Student Manager</h1>
+      <StudentList 
+        students={students}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
     </div>
   );
 }

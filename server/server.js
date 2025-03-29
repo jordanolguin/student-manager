b8acb05586c3e9b0c022ebc0bac78e87e7a2a291
Jordan Olguin <jdolguin62@gmail.com>
2025-03-29 09:49:25 -0600

    created and connected CRUD routes to /students endpoint in server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const db = require("./config/connection");
+const studentRoutes = require('./routes/students');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/students', studentRoutes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
